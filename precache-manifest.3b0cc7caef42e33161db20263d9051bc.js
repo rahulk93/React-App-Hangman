e25072b639fc42a06483f97d0d840df5e5eb7d39
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 14:37:02 +1300

    Updates

---
+++ precache-manifest.3b0cc7caef42e33161db20263d9051bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6917b05edab238df75bdfff081fc7a2",
+    "url": "/React-App-Hangman/index.html"
+  },
+  {
+    "revision": "7aa5b18af62951a118a6",
+    "url": "/React-App-Hangman/static/css/main.0998170a.chunk.css"
+  },
+  {
+    "revision": "6f59bbb8081c238a7397",
+    "url": "/React-App-Hangman/static/js/2.3add00ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Hangman/static/js/2.3add00ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa5b18af62951a118a6",
+    "url": "/React-App-Hangman/static/js/main.10826163.chunk.js"
+  },
+  {
+    "revision": "9ca3ffe1d526dd94f48c",
+    "url": "/React-App-Hangman/static/js/runtime-main.2999ab68.js"
+  }
+]);
